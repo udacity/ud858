bd2d2cc0e4e485f66ca74e3c9565c4d4b29f6463
Gundega Dekena <gundega@udacity.com>
2014-11-12 18:25:08 -0800

    Add the finished version of the Conference Central app

---
+++ ConferenceCentral_Complete/settings.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+"""settings.py
+
+Udacity conference server-side Python App Engine app user settings
+
+$Id$
+
+created/forked from conference.py by wesc on 2014 may 24
+
+"""
+
+# Replace the following lines with client IDs obtained from the APIs
+# Console or Cloud Console.
+WEB_CLIENT_ID = 'replace with Web client ID'
+ANDROID_CLIENT_ID = 'replace with Android client ID'
+IOS_CLIENT_ID = 'replace with iOS client ID'
+ANDROID_AUDIENCE = WEB_CLIENT_ID
