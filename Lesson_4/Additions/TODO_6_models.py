7089ac55206ee2078861df43765368d23d698704
Gundega Dekena <gundega@udacity.com>
2015-03-04 23:21:17 +0200

    fix: transaction quiz ordering and content

---
+++ Lesson_4/Additions/TODO_6_models.py
@@ -0,0 +1,7 @@
+# replace your existing Profile class with this
+class Profile(ndb.Model):
+    """Profile -- User profile object"""
+    displayName = ndb.StringProperty()
+    mainEmail = ndb.StringProperty()
+    teeShirtSize = ndb.StringProperty(default='NOT_SPECIFIED')
+    conferenceKeysToAttend = ndb.StringProperty(repeated=True)
