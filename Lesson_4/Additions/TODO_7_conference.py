9b11f9eee53311749e755148541e1fb104dc0aeb
Gundega Dekena <gundega@udacity.com>
2015-03-05 01:35:17 +0200

    fix: TODO 7 instructions and bug

--- Lesson_4/Additions/TODO_7_conference.py
+++ Lesson_4/Additions/TODO_7_conference.py
@@ -7,12 +7,12 @@
         # step 1: get user profile
         # step 2: get conferenceKeysToAttend from profile.
         # to make a ndb key from websafe key you can use:
-        # ndb.Key(urlsafe=my_websafe_key)
+        # ndb.Key(urlsafe=my_websafe_key_string)
         # step 3: fetch conferences from datastore. 
         # Use get_multi(array_of_keys) to fetch all keys at once.
         # Do not fetch them one by one!
 
         # return set of ConferenceForm objects per Conference
-        return ConferenceForms(items=[self._copyConferenceToForm(conf, names[conf.organizerUserId])\
+        return ConferenceForms(items=[self._copyConferenceToForm(conf, "")\
          for conf in conferences]
         )
