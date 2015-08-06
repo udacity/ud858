ac75c2202b7de4e703790afc0c6889589ca210ac
Jhoon Saravia <jhoonsar@gmail.com>
2015-08-06 16:36:37 -0500

    Lesson 1: added new API Method

--- Lesson_2/000_Hello_Endpoints/helloworld_api.py
+++ Lesson_2/000_Hello_Endpoints/helloworld_api.py
@@ -16,6 +16,7 @@ from protorpc import remote
 REQUEST_CONTAINER = endpoints.ResourceContainer(
     message_types.VoidMessage,
     name=messages.StringField(1),
+    period=messages.StringField(2)
 )
 
 
@@ -40,6 +41,12 @@ class HelloWorldApi(remote.Service):
       path = "sayHelloByName", http_method='GET', name = "sayHelloByName")
     def say_hello_by_name(self, request):
       greet = "Hello {}".format(request.name)
+      return Hello(greeting=greet)
+
+    @endpoints.method(REQUEST_CONTAINER, Hello,
+      path = "greetByPeriod", http_method='GET', name = "greetByPeriod")
+    def greet_by_period(self, request):
+      greet = "Good {} {}".format(request.period, request.name)
       return Hello(greeting=greet)
 
 
