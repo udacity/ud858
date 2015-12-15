428d25a3cf556eae6f8782fda495c7cc5678902e
Greg Quinlan <quinlangl@gmail.com>
2015-12-15 11:57:50 -0500

    Add getConference to Lesson 4 TODO
    Provide getConference code to TODO additions before it is used in the lesson videos.

--- Lesson_4/Additions/TODO_3_conference.py
+++ Lesson_4/Additions/TODO_3_conference.py
@@ -19,3 +19,19 @@ def getConferencesCreated(self, request):
     return ConferenceForms(
         items=[self._copyConferenceToForm(conf, displayName) for conf in conferences]
     )
+    
+
+    @endpoints.method(CONF_GET_REQUEST, ConferenceForm,
+            path='conference/{websafeConferenceKey}',
+            http_method='GET', name='getConference')
+    def getConference(self, request):
+        """Return requested conference (by websafeConferenceKey)."""
+        # get Conference object from request; bail if not found
+        conf = ndb.Key(urlsafe=request.websafeConferenceKey).get()
+        if not conf:
+            raise endpoints.NotFoundException(
+                'No conference found with key: %s' % request.websafeConferenceKey)
+        prof = conf.key.parent().get()
+        # return ConferenceForm
+        return self._copyConferenceToForm(conf, getattr(prof, 'displayName'))
+     
